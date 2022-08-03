f38e5428b655d878fc5631aa51e728bddc60f1d1
Jo-Schorr <101523764+Jo-Schorr@users.noreply.github.com>
2022-08-02 21:48:56 -0300

    commit

--- unity.rb
+++ unity.rb
@@ -1,17 +1,42 @@
-def meters_to(unity, meters)
-  case unity
-  when :miles
-    (meters / 1000.00) / 1.609
-  when :yard
-    meters / 0.914
-  when :inch
-    (meters * 100) / 2.24
-  when :foot
-    meters / 0.3048
-  else
-    "unidade desconhecida"
-  end
+'''    def meters_to(unity, meters)
+      case unity
+      when :miles
+        (meters / 1000.00) / 1.609
+      when :yard
+        meters / 0.914
+      when :inch
+        (meters * 100) / 2.24
+      when :foot
+        meters / 0.3048
+      else
+        "unidade desconhecida"
+      end
+    end
+
+
+    puts meters_to(:foot, 1)
+'''
+
+
+
+unity = :inch
+meters = 34
+
+resultado = case unity
+when :miles
+  (meters / 1000.0) / 1.609
+when :yard
+  meters / 0.914
+when :inch
+  (meters * 100) / 2.54
+
+when :foot
+  meters / 0.3048
+
+else
+  "unidade desconhecida"
+
 end
 
 
- puts meters_to(:foot, 1)
+puts "Valor em polegadas #{resultado}"
