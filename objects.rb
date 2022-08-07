5eec9bb98c88097b55aaf9f973e0f0f608355b93
Josué Schorr <101523764+Jo-Schorr@users.noreply.github.com>
2022-08-07 17:30:18 -0300

    commit

--- objects.rb
+++ objects.rb
@@ -1,14 +1,27 @@
 class Car
+  attr_reader :price
+  attr_accessor :year
 
+
+ def initialize(fuel_type,year,price)
+   @fuel_type =  fuel_type
+   @year = year
+   @price = price
+
+ end
+
+def year= (value)
+ @year = 2000
+ end
 
 end
 
-polo = Car.new
-
-p polo
-
+polo = Car.new(:flex, 2013, 37000)
+passt = Car.new(:gasolina, 2016,28000)
 
 
-testando
-testandoteste
-testandoteste
+
+puts polo.year
+#puts polo.fuel_type
+polo.year = 2012
+puts polo.year
